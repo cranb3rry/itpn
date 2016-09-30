def40fc59a3fbb04eba948a965f37abc67e7b97d
cranb3rry <nine@noeight.net>
2016-09-30 20:00:22 +0300

    deleted:    madlibs_generator.py 	deleted:    test.py 	deleted:    test2.py

--- entertainment_center.py
+++
@@ -1,9 +0,0 @@
-import media
-import fresh_tomatoes
-
-toy_story = media.Movie("Title", "Storyline",
- "http://cdn.collider.com/wp-content/uploads/my-neighbor-totoro-poster-variant-olly-moss-mondo.jpg", "https://www.youtube.com/watch?v=92a7Hj0ijLs")
-
-
-movies = [toy_story]
-fresh_tomatoes.open_movies_page(movies)
